b5472c7e0bd6f523ecccc5c91a7b48c3c23b41b8
ensanguinedIB <johnmconstable@icloud.com>
2022-02-22 22:01:05 +0000

    Button now prompts video play as well

--- scripts/autoplay.js
+++ scripts/autoplay.js
@@ -1,18 +1,20 @@
 window.addEventListener("load", () =>
 {
     /* Attempt to play the audio and video files.  As the video has no sound,
-       it will probably play without a problem.
-       Browsers tend to block autoplay on audio, so if autoplay fails, the user is shown a button.
-       On pressing the button, the audio is tried once more, as interaction usually works. */
+       it will probably play on a desktop, mobile tends to fare less well.
+       Browsers on the whole block autoplay on audio if there is no user interaction.
+       If either fails, the user is shown a button which attempts to play both manually. */
 
     let video = document.getElementById("video");
     let audio = document.getElementById("audio");
+    let play_button = document.getElementById("play-button");
 
     let play_video = video.play();
 
     play_video.then(_ => {}, _error =>
     {
         console.log("Video autoplay failed.");
+        play_button.style.display = "block";
     });
 
     let play_audio = audio.play();
@@ -20,8 +22,6 @@ window.addEventListener("load", () =>
     play_audio.then(_ => {}, _error =>
     {
         console.log("Audio autoplay failed.");
-
-        let play_button = document.getElementById("play-button");
         play_button.style.display = "block";
     });
 });
@@ -29,10 +29,13 @@ window.addEventListener("load", () =>
 
 function play_audio()
 {
-    /* Callback function for the button.  Attempt to play audio, then remove the button. */
+    /* Callback function for the button.  Attempt to play audio and video, then remove the button. */
         
     let audio = document.getElementById("audio");
     audio.play();
+
+    let video = document.getElementById("video");
+    video.play();
 
     let play_button = document.getElementById("play-button");
     play_button.style.display = "none";
